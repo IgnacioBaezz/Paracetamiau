5a6e819e1da309854d04ad9042a27b7630235c62
Raúl Ernesto Sanhueza Manríquez <resm5233@gmail.com>
2025-06-03 01:40:13 -0400

    index-quizz pages

--- frontend_react/src/pages/Index.jsx
+++ frontend_react/src/pages/Index.jsx
@@ -3,21 +3,41 @@ import "../styles/styles.css";
 import "../styles/variables.css";
 import "../styles/backgrounds.css";
 
-
 // Componenetes
 import Navbar from "../components/Navbar";
-import BotonesMenu from "../components/BotonesMenu";
 import LoginForm from "../components/LoginForm";
+import IndexSeccion from "../components/IndexSeccion";
+import InfoCards from "../components/InfoCards";
+
 
 const Index = () => {
   return (
     <>
       <Navbar titulo="Index" />
-      <main className="container-fluid">
-        <div className="row position-relative">
-          <BotonesMenu />
-          <LoginForm />
+      <main className="container-fluid  d-flex align-items-center">
+        <div className="row w-100 ">
+          <div className="col-12 col-md-8 d-flex align-items-center justify-content-center">
+            <IndexSeccion />
+          </div>
+          <div className="col-12 col-md-4 d-flex align-items-center justify-content-center">
+            <LoginForm />
+          </div>
+
+          <div>
+            <InfoCards />
+          </div>
+
+
+
         </div>
+          
+          
+
+          
+      
+          
+          
+          
       </main>
     </>
   );
