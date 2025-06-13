6659cc9c6707e790b632c409981aea7d40b18efb
Nickolle Silva <nickolle.a@gmail.com>
2025-06-13 11:07:52 -0400

    Re-diseño de index y resolución de conflictos

--- frontend_react/src/pages/Index.jsx
+++ frontend_react/src/pages/Index.jsx
@@ -3,9 +3,8 @@ import "../styles/styles.css";
 import "../styles/variables.css";
 import "../styles/backgrounds.css";
 
-// Componenetes
+// Componentes
 import Navbar from "../components/Navbar";
-import LoginForm from "../components/LoginForm";
 import IndexSeccion from "../components/IndexSeccion";
 import InfoCards from "../components/InfoCards";
 
@@ -13,37 +12,12 @@ const Index = () => {
   return (
     <>
       <Navbar titulo="Index" />
-      <main className="container-fluid d-flex align-items-center">
-        <div className="row">
-          <div className="col-12 col-md-8 d-flex align-items-center justify-content-center">
-            <IndexSeccion />
-          </div>
-          <div className="col-12 col-md-4 d-flex align-items-center justify-content-center">
-            <LoginForm />
-          </div>
-          <InfoCards />
-        </div>
+      <main className="container-fluid d-flex flex-column align-items-center p-0">
+        <IndexSeccion />
+        <InfoCards />
       </main>
     </>
   );
 };
 
 export default Index;
-
-<>
-  <Navbar titulo="Index" />
-  <main className="container-fluid d-flex align-items-center">
-    <div className="row w-100 ">
-      <div className="col-12 col-md-8 d-flex align-items-center justify-content-center">
-        <IndexSeccion />
-      </div>
-      <div className="col-12 col-md-4 d-flex align-items-center justify-content-center d-none d-md-block">
-        <LoginForm />
-      </div>
-      <InfoCards />
-      <div className="col-12 col-md-4 d-flex align-items-center justify-content-center d-block d-md-none">
-        <LoginForm />
-      </div>
-    </div>
-  </main>
-</>;
