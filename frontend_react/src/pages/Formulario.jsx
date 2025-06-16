764f56acbde657c71925dbd7d7d02775293f3c2f
Raúl Ernesto Sanhueza Manríquez <resm5233@gmail.com>
2025-06-16 10:11:49 -0400

    page formulario

---
+++ frontend_react/src/pages/Formulario.jsx
@@ -0,0 +1,35 @@
+// Estilos generales
+import "../styles/styles.css";
+import "../styles/variables.css";
+import "../styles/backgrounds.css";
+
+// Componentes
+import { useState } from 'react'
+import Login from "../components/login";
+import Signup from "../components/Signup";
+import Navbar from "../components/Navbar";
+
+const Formulario = () => {
+    const [modo, setModo] = useState("signup");
+
+    const cambiarModo = () => {
+        setModo(modo === "login" ? "signup" : "login");
+    };
+
+    return (
+        <>
+        <Navbar />
+        <div className="d-flex justify-items-center align-items-center">
+        <div className="container mt-5 w-100 bg-color5 rounded-5" style={{maxWidth: "700px"}}>
+            {modo === "login" ? (
+                <Login cambiarModo={cambiarModo} />
+            ) : (
+                <Signup cambiarModo={cambiarModo} />
+            )}
+        </div>
+        </div>
+        </>
+    );
+};
+
+export default Formulario
