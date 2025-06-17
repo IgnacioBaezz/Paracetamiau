3051542854fd2dfeb2be6a31d01772721d42f568
Ignacio Baez <ibaezg3@gmail.com>
2025-06-17 18:45:07 -0400

    AuthContext en las rutas

--- frontend_react/src/components/Navbar.jsx
+++ frontend_react/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import logo from "./../assets/img/Paracetamiau.png";
 import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext.jsx";
 
 const Navbar = ({ titulo, mostrarLogout }) => {
   const navigate = useNavigate();
+  const { logout } = useAuth();
 
-  const handleLogout = () => {
-    localStorage.removeItem("token"); // eliminamos el token de sesión del usuario
-    navigate("/"); // nos redirige al index
+  const handleLogout = async () => {
+    await logout();
+    navigate("/");
   };
+
   return (
     <nav className="navbar bg-color7 sticky-top">
       <div className="container">
@@ -17,7 +20,10 @@ const Navbar = ({ titulo, mostrarLogout }) => {
         <h2 className="text-uppercase fw-bold mx-auto">{titulo}</h2>
         {/* Botón de cierre */}
         {mostrarLogout && (
-          <button className="btn bg-color8 ms-3 shadow-sm" onClick={handleLogout}>
+          <button
+            className="btn bg-color8 ms-3 shadow-sm"
+            onClick={handleLogout}
+          >
             Cerrar sesión
           </button>
         )}
--- frontend_react/src/pages/DiarioContenido.jsx
+++ frontend_react/src/pages/DiarioContenido.jsx
@@ -10,26 +10,22 @@ import Navbar from "../components/Navbar";
 import BotonesMenu from "../components/BotonesMenu";
 import Animaciones from "../components/Animaciones";
 
-
 const DiarioContenido = () => {
-    return (
-       <>
+  return (
+    <>
       <Navbar titulo="Diario Bienestar" mostrarLogout={true} />
       <main className="container-fluid">
         <div className="position-relative">
           <BotonesMenu />
-        <div className="col-12">
+          <div className="col-12">
             <div className="row justify-content-around  mt-5 ms-5">
-                <Animaciones />
+              <Animaciones />
             </div>
-        </div>
-
+          </div>
         </div>
       </main>
     </>
-
-
-    )
-}
+  );
+};
 
 export default DiarioContenido;
